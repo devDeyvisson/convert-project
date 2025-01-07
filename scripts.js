09811d4f1144db8e1107f189cd54a0faf51b932b
Deyvisson Menezes <deyvissonsantos3348@gmail.com>
2025-01-07 17:48:46 -0300

    obtendo a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
+// Obtendo os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
-//Manipulando o input amount para receber somente números
+// Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
     const hasCaractersRegex = /\D+/g
     amount.value = amount.value.replace(hasCaractersRegex, "")
 })
+
+// Capturando o evento de submit (enviar) do formulário
+form.onsubmit = (event) => {
+    //Evita o carregamento da página
+    event.preventDefault()
+
+    console.log(currency.value)
+}
