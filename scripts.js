b79294b9584740c175b3630e02b947b9aa63a8b5
Deyvisson Menezes <deyvissonsantos3348@gmail.com>
2025-01-08 10:49:03 -0300

    identificando a moeda

--- scripts.js
+++ scripts.js
@@ -3,6 +3,12 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 
+// Cotação de moedas do dia
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
     const hasCaractersRegex = /\D+/g
@@ -14,5 +20,20 @@ form.onsubmit = (event) => {
     //Evita o carregamento da página
     event.preventDefault()
 
-    console.log(currency.value)
+    switch(currency.value) {
+        case "USD": 
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR": 
+            convertCurrency(amount.value, EUR, "€")   
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")  
+            break
+    }
+}
+
+// Convertendo a moeda
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
 }
