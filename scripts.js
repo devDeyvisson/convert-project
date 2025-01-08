7c7537ddb2d0d1057af5da16b8d489e7f753d898
Deyvisson Menezes <deyvissonsantos3348@gmail.com>
2025-01-08 11:09:55 -0300

    exibindo o footer que mostra o resultado temporário

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 // Cotação de moedas do dia
 const USD = 4.87
@@ -17,17 +18,17 @@ amount.addEventListener("input", () => {
 
 // Capturando o evento de submit (enviar) do formulário
 form.onsubmit = (event) => {
-    //Evita o carregamento da página
+    // Evita o carregamento da página
     event.preventDefault()
 
     switch(currency.value) {
-        case "USD": 
+        case "USD": // Dólar
             convertCurrency(amount.value, USD, "US$")
             break
-        case "EUR": 
+        case "EUR": // Euro
             convertCurrency(amount.value, EUR, "€")   
             break
-        case "GBP":
+        case "GBP": // Libra
             convertCurrency(amount.value, GBP, "£")  
             break
     }
@@ -35,5 +36,15 @@ form.onsubmit = (event) => {
 
 // Convertendo a moeda
 function convertCurrency(amount, price, symbol) {
-    console.log(amount, price, symbol)
+    try {
+        // Aplica a classe que exibe o footer para mostrar o resultado na tela
+        footer.classList.add("show-result")
+    } catch (error) {
+        // Remove a classe footer, ou seja, não exibe o resultado na tela
+        footer.classList.remove("show-result")
+        
+        console.log(error)
+        alert("Não foi possível fazer a conversão. Tente novamente mais terde!")
+    }
+    // console.log(amount, price, symbol)
 }
